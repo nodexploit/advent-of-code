f3a29a7d3f49ccdc5e13f2443ebce44f742bfb3c
Nicolas Vargas Ortega <nvortega92@gmail.com>
2022-12-01 12:56:24 +0100

    Solve day 1 problem 1

--- 2022/day1/go/problem1.go
+++ 2022/day1/go/problem1.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	calories, err := readInput()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var elfSumCalories []int
+	var elfSum int
+	for _, calorie := range calories {
+		if calorie == -1 {
+			elfSumCalories = append(elfSumCalories, elfSum)
+			elfSum = 0
+		} else {
+			elfSum = elfSum + calorie
+		}
+	}
+
+	var max int
+	for _, sum := range elfSumCalories {
+		if sum > max {
+			max = sum
+		}
+	}
+	fmt.Println(max)
+}
+
+// readInput loads the input file with -1 for whitespaces in a slice
+func readInput() ([]int, error) {
+	file, err := os.Open("../input.txt")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var calories []int
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		calorie, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			calories = append(calories, -1)
+		} else {
+			calories = append(calories, calorie)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return calories, nil
 }
